2f456759042a875d26414c165ef970774b9a4d0a
Randy Girard <randy@tradepending.com>
2021-04-30 15:51:31 -0400

    Add timezonse.

--- bot.rb
+++ bot.rb
@@ -30,7 +30,6 @@ DB = Sequel.connect(DATABASE_URL)
 
 Sequel.extension :migration
 Sequel::Migrator.run(DB, 'db/migrate', :use_transactions=>true)
-Sequel.default_timezone = :etc
 
 class Timer < Sequel::Model
 end
