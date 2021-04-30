cd33eccadc362ae76e17cbabb61b908f7412370e
Randy Girard <randy@tradepending.com>
2021-04-30 14:11:04 -0400

    Fix output.

--- bot.rb
+++ bot.rb
@@ -340,7 +340,7 @@ BOT.command(:show) do |event, *args|
 
   timers = Timer.where(Sequel.ilike(:name, "#{mob.to_s}%")).all
   if timers.size > 1
-    event.respond "Request turned multiple results: `#{timers.map(&:name).join(", ")}`. Please be more specific."
+    event.respond "Request returned multiple results: #{timers.map {|timer| "`#{timer.name}`" }.join(", ")}. Please be more specific."
   elsif timers.size == 1
     timer = timers
     show_message(event, timer)
