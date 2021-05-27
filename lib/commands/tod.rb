5155067f55764d5d4a506d6583f5891f784b1797
Randy Girard <randy@tradepending.com>
2021-05-26 21:22:05 -0400

    Better handling of tod

--- lib/commands/tod.rb
+++ lib/commands/tod.rb
@@ -27,7 +27,22 @@ BOT.command(:tod) do |event, *args|
     return
   end
 
-  mob, manual_tod = args.join(" ").split(/[\|\,]/)
+  mob = ""
+  manual_tod = ""
+
+  # look for time
+  arguments = args.join(" ")
+  mob, manual_tod = arguments.split(/[\|\,]/)
+  if manual_tod == nil
+    matches = arguments.to_s.downcase.match(/(.*?)\s+([0-9]|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(.*?)$/)
+    puts matches.inspect
+    if matches && matches[1] && matches[2]
+      mob = matches[1]
+      manual_tod = [matches[2], matches[3]].compact.join
+    end
+  end
+
+
   mob.strip!
   manual_tod.strip! if manual_tod
 
