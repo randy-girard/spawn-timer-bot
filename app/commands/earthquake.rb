ae24d1488bb1c3775816d54c88fa10364ee49afd
Addison <addison.rich@gmail.com>
2022-04-20 20:40:18 -0400

    clarified comment

--- app/commands/earthquake.rb
+++ app/commands/earthquake.rb
@@ -12,7 +12,7 @@ BOT.command(:earthquake) do |event|
   event.respond "Earthquake has been registered!"
   BOT.send_message(TIMER_ALERT_CHANNEL_ID, "**EARTHQUAKE**")
   if defined? (EARTHQUAKE_ALERT_CHANNEL_ID) && EARTHQUAKE_ALERT_CHANNEL_ID.to_s.strip.length > 0
-    # earthquake alert channel is defined, so send the optional EARTHQUAKE_ALERT_MESSAGE if defined, otherwise send the default
+    # earthquake alert channel is defined, so send the EARTHQUAKE_ALERT_MESSAGE if defined, otherwise send the default alert message
     BOT.send_message(EARTHQUAKE_ALERT_CHANNEL_ID, EARTHQUAKE_ALERT_MESSAGE.empty? ? "EARTHQUAKE" : EARTHQUAKE_ALERT_MESSAGE)
   end
 end
