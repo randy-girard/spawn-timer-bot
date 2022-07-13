4938b6e1af55ffcbee86e8dd9276f475e05c5551
Randy Girard <randy@tradepending.com>
2022-07-13 13:25:07 -0400

    Move into if block

--- app/commands/timers.rb
+++ app/commands/timers.rb
@@ -5,18 +5,17 @@ BOT.command(:timers) do |event|
 
   timers = Timer.all.select {|t| t.name.to_s.length > 0 }.map(&:name).sort
 
-  char_length = 0
-  num_timers = 0
-  timers.each do |timer|
-    char_length += timer.length
-    if char_length > 2_000
-      break
-    end
-    num_timers += 1
-  end
-
   timers_string = if event.channel.type == 1
-                    timers[0..(num_timers-1)].join("\n")
+                    char_length = 0
+                    num_timers = 0
+                    timers.each do |timer|
+                      char_length += timer.length
+                      if char_length > 2_000
+                        break
+                      end
+                      num_timers += 1
+                    end
+                    timers[0..(num_timers-2)].join("\n")
                   else
                     timers.join(", ").truncate(1950, omission: "...")
                   end
