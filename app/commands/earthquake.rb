b70ebc99a99ced5525c94ce577e80f3d49d8e576
Randy Girard <randy@tradepending.com>
2022-07-17 16:45:28 -0400

    Rename earthquake to quake, but support earthquake too

--- app/commands/earthquake.rb
+++ app/commands/earthquake.rb
@@ -1,4 +1,12 @@
 BOT.command(:earthquake) do |event|
+  command_quake(event)
+end
+
+BOT.command(:quake) do |event|
+  command_quake(event)
+end
+
+def command_quake(event)
   return if event.channel.id != COMMAND_CHANNEL_ID
 
   Timer.all.each do |timer|
@@ -9,10 +17,10 @@ BOT.command(:earthquake) do |event|
     timer.save
   end
   update_timers_channel
-  event.respond "Earthquake has been registered!"
-  BOT.send_message(TIMER_ALERT_CHANNEL_ID, "**EARTHQUAKE**")
+  event.respond "Quake has been registered!"
+  BOT.send_message(TIMER_ALERT_CHANNEL_ID, "**QUAKE**")
   if defined? (EARTHQUAKE_ALERT_CHANNEL_ID) && EARTHQUAKE_ALERT_CHANNEL_ID.to_s.strip.length > 0
     # earthquake alert channel is defined, so send the EARTHQUAKE_ALERT_MESSAGE if defined, otherwise send the default alert message
-    BOT.send_message(EARTHQUAKE_ALERT_CHANNEL_ID, EARTHQUAKE_ALERT_MESSAGE.empty? ? "EARTHQUAKE" : EARTHQUAKE_ALERT_MESSAGE)
+    BOT.send_message(EARTHQUAKE_ALERT_CHANNEL_ID, EARTHQUAKE_ALERT_MESSAGE.empty? ? "QUAKE" : EARTHQUAKE_ALERT_MESSAGE)
   end
 end
