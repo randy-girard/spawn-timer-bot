4464e83b436cfe5b8486135680e8728efdc178e3
Randy Girard <randy@tradepending.com>
2022-07-18 12:37:26 -0400

    Add am/pm

--- app/commands/schedule.rb
+++ app/commands/schedule.rb
@@ -23,7 +23,7 @@ BOT.command(:schedule) do |event, *args|
     if timers.size > 0
       output << "**#{day.strftime("%A %-m/%-d")}**"
       timers.each do |timer|
-        output << "#{timer[:timer].name} - #{timer[:time].strftime("%I:%m")} EST"
+        output << "#{timer[:timer].name} - #{timer[:time].strftime("%I:%m %p")} EST"
       end
       output << ""
     end
