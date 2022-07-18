0c0c05fdedbd25246451ca50f8dc94a35952ef74
Randy Girard <randy@tradepending.com>
2022-07-17 20:59:09 -0400

    Adds a webapp calendar

--- webapp.rb
+++ webapp.rb
@@ -21,6 +21,80 @@ require_relative 'app/helpers/timer'
 
 set :views, Proc.new { File.join(root, "app", "views") }
 
+get "/schedule" do
+  events = Timer.all.map {|timer|
+    hash = {
+      title: timer.name,
+      start: next_spawn_time_start(timer.name)
+    }
+    hash[:end] = next_spawn_time_end(timer.name)
+
+    if timer.auto_tod
+      hash[:groupId] = timer.id
+    end
+
+    hash
+  }
+  #
+  #   }[
+  #   {
+  #     title: 'All Day Event',
+  #     start: '2020-09-01'
+  #   },
+  #   {
+  #     title: 'Long Event',
+  #     start: '2020-09-07',
+  #     end: '2020-09-10'
+  #   },
+  #   {
+  #     groupId: 999,
+  #     title: 'Repeating Event',
+  #     start: '2020-09-09T16:00:00'
+  #   },
+  #   {
+  #     groupId: 999,
+  #     title: 'Repeating Event',
+  #     start: '2020-09-16T16:00:00'
+  #   },
+  #   {
+  #     title: 'Conference',
+  #     start: '2020-09-11',
+  #     end: '2020-09-13'
+  #   },
+  #   {
+  #     title: 'Meeting',
+  #     start: '2020-09-12T10:30:00',
+  #     end: '2020-09-12T12:30:00'
+  #   },
+  #   {
+  #     title: 'Lunch',
+  #     start: '2020-09-12T12:00:00'
+  #   },
+  #   {
+  #     title: 'Meeting',
+  #     start: '2020-09-12T14:30:00'
+  #   },
+  #   {
+  #     title: 'Happy Hour',
+  #     start: '2020-09-12T17:30:00'
+  #   },
+  #   {
+  #     title: 'Dinner',
+  #     start: '2020-09-12T20:00:00'
+  #   },
+  #   {
+  #     title: 'Birthday Party',
+  #     start: '2020-09-13T07:00:00'
+  #   },
+  #   {
+  #     title: 'Click for Google',
+  #     url: 'http://google.com/',
+  #     start: '2020-09-28'
+  #   }
+  # ]
+  erb :"schedule/index", { :locals => { events_json: events.to_json } }
+end
+
 get "/timers" do
   any_in_window = false
   any_need_tod = false
