ea836a666625ec85a8add7ce258aa30dc2f01b2f
Randy Girard <rgirard59@yahoo.com>
2026-06-20 09:04:59 -0400

    chore: dont ignore bots

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,4 +1,5 @@
 BOT = Discordrb::Commands::CommandBot.new token: TOKEN,
                                           client_id: CLIENT_ID,
                                           prefix: '!',
+                                          ignore_bots: false,
                                           name: ENV["BOT_NAME"].to_s
